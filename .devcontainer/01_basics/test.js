5f65c62e7f07341df60bf464cbe5de6c672d0c74
Xdencs <108237597+Xdencs@users.noreply.github.com>
2025-10-03 22:20:40 +0000

    dataType

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,21 @@
-console.log("Aashu")
+const accountId = 1443
+let accountEmail = "Aashu@123"
+var accountPassword = "12345"
+accountCity ="Pryagraj"
+let accountstate;
+
+//accountId = 2 not allowed
+
+
+
+accountEmail="naruzee476@gmail.com"
+accountPassword= "00000"
+accountCity="bangluru"
+ console.log(accountId);
+
+ /*
+ Prefer not to use var
+ because of issue in block scope and functional scope
+ */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
---
+++ .devcontainer/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+console.log("Aashu")
+//alert("hello")
+
+let name ="Aashu"
+let age =18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//bolean => true/false
+// null => standalone value
+// undefined => 
+// symbol =>uniqueness
+// object=> 
+
+    console.log(typeof state);
+    
