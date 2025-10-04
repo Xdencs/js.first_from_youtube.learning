affd5a158ce47188baa5e20fbf0adea09eddfda0
Aashu <108237597+Xdencs@users.noreply.github.com>
2025-10-04 14:55:33 +0000

    conversion of operation

---
+++ .devcontainer/03_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0=>false
+// "" => false
+//"Aashu" => true
